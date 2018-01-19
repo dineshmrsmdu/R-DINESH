0c53c893e5449f104255b43e510778ccfa12cf39
dineshmrsmdu <35331305+dineshmrsmdu@users.noreply.github.com>
2018-01-19 15:59:50 +0530

    Create maximum.c

---
+++ maximum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+int a,b[a],i,max;
+scanf("%d",&a);
+for(i=0;i<a;i++)
+{
+scanf("%d",&b[i]);
+if(i==0)
+max=b[i];
+if(max<b[i])
+max=b[i];
+}
+printf("%d",max);
+}
