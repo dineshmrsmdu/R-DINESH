76a913870f7f0e43a90c0f2f32547b2d410d0567
dineshmrsmdu <35331305+dineshmrsmdu@users.noreply.github.com>
2018-01-19 15:57:23 +0530

    Create arithmetic.c

---
+++ arithmetic.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+int i,a1,n,sum,t1,d;
+printf("enter the first element");
+scanf("%d",&a1);
+printf(enter the number of terms");
+scanf("%d",&n);
+printf("enter the difference");
+scanf("%d",&d);
+sum=n(2a1+(n-1)*d)/2;
+t1=a1+(n-1)*d;
+{
+for(i=a1;i<=t1;i+d)
+{
+if(i!=t10
+{
+printf("%d+",i)
+}
+else
+{
+printf("%d,%d",&i,&sum);
+retuen 0;
+}
+}
+}
+}
