5f94ed4c558494a54b81ae1ddd399529c7685c67
dineshmrsmdu <35331305+dineshmrsmdu@users.noreply.github.com>
2018-01-19 16:02:17 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int a[50],i,n,min;
+printf("enter the range");
+scanf("%d",&n);
+printf("enter the array values");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+min=a[0];
+for(i=0;i<n;i++)
+{
+if(a[i]<min)
+{
+min=a[i];
+}
+}
+printf("%d",min);
+return 0;
+}
