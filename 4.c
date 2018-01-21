745d61f28416c1740c03bc41fe5aeeac37940dbf
dineshmrsmdu <35331305+dineshmrsmdu@users.noreply.github.com>
2018-01-21 14:45:39 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char t[30];
+char k;
+int i;
+clrscr();
+scanf("%s",t);
+for(i=0;t[i]!=NULL;i++)
+{
+if(t[i]==k)
+{
+printf("the position is:%d",i);
+}
+}
+return 0;
+}
