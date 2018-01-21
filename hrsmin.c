537d75cc1d2d774760f0cb7e0f0062665a143ba1
dineshmrsmdu <35331305+dineshmrsmdu@users.noreply.github.com>
2018-01-21 13:56:50 +0530

    Create hrsmin.c

---
+++ hrsmin.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+ printf("enter the hrs and min");
+ scanf("%d:%d",&t1);
+ printf("enter the hrs and min");
+ scanf("%d:%d",&t2);
+ res=printf("the result is %d:%d",t1-t2);
+ return 0;
+ }
