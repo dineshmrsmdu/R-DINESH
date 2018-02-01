1599ff517e0cfa5267d417e46798e137fb4e1fb6
dineshmrsmdu <35331305+dineshmrsmdu@users.noreply.github.com>
2018-02-01 14:41:47 +0530

    Rename swap.c to swapa.c

---
+++ swapa.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a=65;b=54;
+printf("value of a and b before swapping %d%d",a,b);
+t=a;
+a=b;
+b=t;
+printf("value of a and b after swapping %d%d",a,b);
+return 0;
+}
