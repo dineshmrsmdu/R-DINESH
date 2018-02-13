6e475004c32f8b8fc0ef97ee1d7d71db71242f7f
dineshmrsmdu <35331305+dineshmrsmdu@users.noreply.github.com>
2018-02-13 14:20:37 +0530

    Create hellomid.c

---
+++ hellomid.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[100];
+    int i,l,n;
+    printf("enter the string:\n");
+    scanf("%s",&a);
+    l=strlen(a);
+    n='*';
+    a[l/2]=n;
+    if(l%2==0)
+    {
+        a[(l-1)/2]=n;
+    }
+    printf("%s",a);
+    return 0;
+}
