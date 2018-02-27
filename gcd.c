b50e004dbc6c9121db074b8b9d7397be71ed8938
dineshmrsmdu <35331305+dineshmrsmdu@users.noreply.github.com>
2018-02-27 15:46:24 +0530

    Create gcd.c

---
+++ gcd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int gcd,n1,n2,i;
+    printf("enter two numbers");
+    scanf("%d%d",&n1,&n2);
+    for(i=1;i<=n1&&i<=n2;++i)
+    {
+        if(n1%i==0&&n2%i==0)
+        gcd=i;
+    }
+    printf("gcd is %d",gcd);
+}
