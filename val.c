ad7193a8943c5f37ec1cd3c9b95a377e5f39b412
dineshmrsmdu <35331305+dineshmrsmdu@users.noreply.github.com>
2018-03-01 15:52:38 +0530

    Create val.c

---
+++ val.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+   int n,k,i;
+   scanf("%d %d",&n,&k);
+   int a[n];
+   for(i=0;i<n;i++)
+   {
+       scanf("%d",&a[i]);
+   }
+   printf("%d",a[k-1]);
+   {
+return 0;
+}
+}
