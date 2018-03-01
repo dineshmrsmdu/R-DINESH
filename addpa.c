c84dc2646bb80c2e9cdef3cdb7958f0dc27d75c8
dineshmrsmdu <35331305+dineshmrsmdu@users.noreply.github.com>
2018-03-01 15:36:26 +0530

    Create addpa.c

---
+++ addpa.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void) 
+{
+	int a[4],sum=0,i;
+	for(i=0;i<5;i++)
+	scanf("%d",&a[i]);
+	for(i=0;i<5;i++)
+	{
+	sum=sum+a[i];
+	}
+	printf("%d",sum);
+  {
+	return 0;
+}
+}
