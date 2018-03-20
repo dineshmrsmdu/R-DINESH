bbca86bb6cacdfd4ec7ca78eb2fe701105e22ae8
dineshmrsmdu <35331305+dineshmrsmdu@users.noreply.github.com>
2018-03-20 15:49:32 +0530

    Create twoadd.c

---
+++ twoadd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n,rev=0,rem,sum=0;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        rem=n%10;
+        sum=sum+(rem*rem);
+        n=n/10;
+    }
+    printf("%d",sum);
+{
+	return 0;
+}
+}
